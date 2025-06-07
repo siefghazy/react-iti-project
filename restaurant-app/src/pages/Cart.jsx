e6e345576eac63d7788d807bc5bd706bee8d3298
siefghazy <133509228+siefghazy@users.noreply.github.com>
2025-06-07 21:40:47 +0300

    Update Cart.jsx

--- restaurant-app/src/pages/Cart.jsx
+++ restaurant-app/src/pages/Cart.jsx
@@ -4,61 +4,55 @@ import CartItems from '../components/cartItems';
 import { v4 as uuidv4 } from 'uuid';
 import Total from '../components/Total';
 const Cart = () => {
-  let sum = 0;
     const [cart, setCart] = useState([]);
-    const[totalPrice,setTotalPrice]=useState(0)
+    const removeFromCart=(id)=>{
+      const updatedCart=cart.filter((i)=>i["id"]!=id)
+      localStorage.setItem("cart",JSON.stringify(updatedCart))
+      setCart(updatedCart)
+    }
     useEffect(() => {
-      let sum=0
       const storedCart=localStorage.getItem("cart")
       const storedOrders = localStorage.getItem("orders");
       if(storedCart){
         if(storedOrders){
-          const newCart=[...JSON.parse(storedCart),...JSON.parse(storedOrders)]
-          for(let i of newCart){
-            sum+=i.totalPrice*i.quantity
-          }
-          setTotalPrice(sum)
+          const parsedOrder=JSON.parse(storedOrders)
+          parsedOrder[0]["id"]=uuidv4()
+          const newCart=[...JSON.parse(storedCart),...parsedOrder]
           setCart(newCart)
           localStorage.removeItem("cart")
           localStorage.removeItem("orders")
           localStorage.setItem("cart",JSON.stringify(newCart))
         }
         else{
-          const parsedCart=JSON.parse(storedCart)
-          for(let i of parsedCart){
-            sum+=i.totalPrice*i.quantity
-          }
           setCart([...JSON.parse(storedCart)])
-          setTotalPrice(sum)
         }
       }
       else{ 
         if (storedOrders) {
           const parsedOrder=JSON.parse(storedOrders)
-          for(let i of parsedOrder){
-            sum+=i.quantity*i.totalPrice
-          }
-          const cart=[...JSON.parse(storedOrders)]
-          setCart([...JSON.parse(storedOrders)]); 
-          setTotalPrice(sum)
+          parsedOrder[0]["id"]=uuidv4()
+          const cart=[...parsedOrder]
+          setCart([...cart]); 
           localStorage.setItem("cart",JSON.stringify(cart))
           localStorage.removeItem("orders")
         } 
       }
 
     }, []);
+    const total = cart.reduce((sum, item) => sum + item.totalPrice, 0);
     return (
         <div className={styling.fluid}>
             <div className={styling.fluidWrapper}>
                 <h1 className={styling.header}>YOUR CART</h1>
                 {
                   cart.map((i)=>{
-                    return <CartItems key={uuidv4()} itemIng={i["ingredients"]} quantity={i["quantity"]} itemPrice={i["totalPrice"]} ></CartItems>
+                    return <CartItems id={i["id"]}  key={i["id"]} remove={removeFromCart} itemIng={i["ingredients"]} quantity={i["quantity"]} itemPrice={i["totalPrice"]} ></CartItems>
                   })
+
                 }
                
             </div>
-            <Total finalPrice={totalPrice}></Total>
+            <Total finalPrice={total}></Total>
         </div>
     );
 }
